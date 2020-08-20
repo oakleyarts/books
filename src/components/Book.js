ed94230dd654bc98ad6c9396a0313440dcff211a
John Galantini <oakleyarts@googlemail.com>
2020-08-20 13:30:18 +0100

    Added Book component

---
+++ src/components/Book.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+
+const Book = (props) => {
+  const {
+    book_author,
+    book_publication_city,
+    book_publication_country,
+    book_publication_year,
+    book_pages,
+    book_title,
+    id
+  } = props;
+
+  return (
+    <Card id={id}>
+      <Card.Body>
+        <Card.Title>{book_title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{book_author.toString()}</Card.Subtitle>
+        <Card.Text>
+          Number of pages: {book_pages}
+          Publication year: {book_publication_year}
+          Publication place: {` ${book_publication_city}, ${book_publication_country} `}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Book.propTypes = {
+  book_author: PropTypes.array.isRequired,
+  book_publication_city: PropTypes.string.isRequired,
+  book_publication_country: PropTypes.string.isRequired,
+  book_publication_year: PropTypes.string.isRequired,
+  book_pages: PropTypes.number.isRequired,
+  book_title: PropTypes.string.isRequired
+};
+
+export default Book;
